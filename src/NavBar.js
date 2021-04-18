097ecfce3623f78a5c55a085116e4cb30642ca19
Ana- Becerril <ana.becbel@gmail.com>
2021-04-17 21:31:07 -0500

    I'm doing practices about React, today learned about state and hooks

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const NavBar = ({title, searchBox, onButtonClicked}) => (
+const NavBar = ({title, searchBox, onButtonClicked}) => {
+    const [input, setInput] = useState("nada aún");
+
+    return ( 
     <div> 
         <h1>{title}</h1>
-        <input placeholder={searchBox}/>
+        <input placeholder={searchBox} onChange={(e) => setInput(e.target.value)}/>
         <button onClick={onButtonClicked}>Search</button>
+        <p>Adivinaré lo que estás escribiendo:{input}</p>
     </div>
-)
+)};
+
 
 export default NavBar;
