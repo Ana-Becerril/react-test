966e90f72074fbac8ebf8aadbb3e13a43b846ced
Ana- Becerril <ana.becbel@gmail.com>
2021-04-21 23:21:32 -0500

    I'm doing practices about React, today learned about the differences between functional and class components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,118 @@
-import React, { Component } from "react";
+//import React, { Component } from "react";
+//import Overview from "./components/Overview";
+//
+//
+//class App extends Component {
+//  constructor() {
+//    super();
+//
+//    this.state = {
+//      task: { text: '' },
+//      tasks: [],
+//    };
+//    this.deleteItem = this.deleteItem.bind(this);
+//  }
+//
+  //
+  //  handleChange = (e) => {
+  //    this.setState({
+  //      task: {
+  //        text: e.target.value,
+  //      }
+  //    });
+  //  };
+//
+//  onSubmitTask = (e) => {
+//    e.preventDefault();
+//    this.setState({
+//      tasks: this.state.tasks.concat(this.state.task),
+//      task: { text: '' },
+//    });
+//  };
+//
+//    deleteItem(index){
+//  this.setState((prevState) => ({
+//    tasks: prevState.tasks.filter((_, i) => i !== index)
+//  }));
+//}
+//
+//  render() {
+//    const { task, tasks } = this.state;
+//
+//
+//    return (
+//      <div>
+//
+//        <form onSubmit={this.onSubmitTask}>
+//          <label htmlFor="taskInput">Enter task</label>
+//          <input
+//            onChange={this.handleChange}
+//            value={this.state.task.text}
+//            type="text"
+//            id="taskInput"
+//          />
+//          <button type="submit">
+//            Add Task
+//          </button>
+//        </form>
+//        <Overview tasks={tasks} deleteItem={this.deleteItem} />
+//      </div>
+//    );
+//  }
+//}
+//
+//export default App;
+
+import React, { useState } from "react";
 import Overview from "./components/Overview";
+import uniqid from "uniqid";
 
 
-class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      task: { text: '' },
-      tasks: [],
-    };
-    this.deleteItem = this.deleteItem.bind(this);
-  }
+function App() {
 
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-      }
-    });
+  const handleChange = e => {
+    setTask({ text: e.target.value, id: uniqid() });
   };
 
-  onSubmitTask = (e) => {
+  const onSubmitTask = (e) => {
     e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: { text: '' },
-    });
+    const tasksClone=[...tasks];
+    tasksClone.push(task)
+    setTasks(tasksClone);
+    setTask({ text: '' }) 
   };
 
-    deleteItem(index){
-  this.setState((prevState) => ({
-    tasks: prevState.tasks.filter((_, i) => i !== index)
-  }));
-}
-
-  render() {
-    const { task, tasks } = this.state;
-
-
-    return (
-      <div>
-
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task</label>
-          <input
-            onChange={this.handleChange}
-            value={this.state.task.text}
-            type="text"
-            id="taskInput"
-          />
-          <button type="submit">
-            Add Task
-          </button>
-        </form>
-        <Overview tasks={tasks} deleteItem={this.deleteItem} />
-      </div>
-    );
+  const deleteItem = (index) => {
+    console.log(tasks[0])
+    console.log(index)
+    const tasksClone=[...tasks];
+    tasksClone.filter( task => task.id !== index)
+    setTasks(tasksClone)
   }
-}
 
-export default App;
+  //var newArray = arr.filter(callback(currentValue[, index[, array]])[, thisArg])
+
+
+  return (
+    <div>
+      <form onSubmit={onSubmitTask}>
+        <label htmlFor="taskInput">Enter task</label>
+        <input
+          onChange={handleChange}
+          value={task.text}
+          type="text"
+          id="taskInput"
+        />
+        <button type="submit">
+          Add Task
+                </button>
+      </form>
+      <Overview tasks={tasks} deleteItem={deleteItem}/>
+    </div>
+  );
+};
+export default App
